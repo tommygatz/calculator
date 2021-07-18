30e43914f67520a0588f9f97d26656ae89d95d9b
tommygatz <grime.thomas@gmail.com>
2021-07-17 22:00:30 -0400

    rebuilding structure to host on github

--- Archive/Calculator/js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('answer').innerHTML = prod;
-
-function testFunc() {
-  p1 = document.getElementById('p1').value;
-  p2 = document.getElementById('p2').value;
-  prod = p1 * p2;
-  document.getElementById('answer').innerHTML = prod;
-}
--- Archive/index.php
+++
@@ -1,1 +0,0 @@
-<?php echo "tommy gatz' test site"; ?>
