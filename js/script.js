0b4525df198ea3b8891bf698dc7dbf1ad7d12c39
tommygatz <grime.thomas@gmail.com>
2021-07-25 00:34:53 -0400

    number buttons working (kinda) and displaying

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+let display = 0;
+let savedNumber = 0;
+let currentNumber = 0;
+let result = 0;
+let operator = "";
+
+
+// function to update display
+function updateDisplay(input){
+  document.getElementById("displayText").innerHTML = input;
+}
+
+
+// clear screen
+function clearScreen(){
+  display = 0;
+  currentNumber = 0;
+  updateDisplay(display);
+}
+
+// pull numbers from buttons pressed
 function sendVal(value){
-  console.log(typeof value);
+  currentNumber = (currentNumber*10) + value;
+  updateDisplay(currentNumber)
+}
+
+// functions for math operations
+function add(a,b){
+  return a + b;
+}
+function subt(a,b){
+  return a - b;
+}
+function mult(a,b){
+  return a * b;
+}
+function divide(a,b){
+  return a / b;
+}
+
+// evaluate math problem when = is pressed using switch cases
+
+function equals(){
+
 }
